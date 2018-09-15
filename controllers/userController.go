6341e84a4f6c6ffd0db6a82ebff48a27ddeec5e2
ChallenAi <jianglun465@sina.com>
2018-09-15 23:51:15 +0800

    response rest style data

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,18 +1,30 @@
 package controllers
 
 import (
-	"fmt"
+	"encoding/json"
+	// "fmt"
+	"github.com/ChallenAi/iTools-service/models"
 	"github.com/valyala/fasthttp"
 )
 
 func GetAllUsers(ctx *fasthttp.RequestCtx) {
-	fmt.Println("test")
+	user := &models.User{
+		UserId:   1,
+		UserName: "Challen",
+	}
+	resp, _ := json.Marshal(user)
+
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	ctx.SetContentType("application/json")
+	ctx.Write(resp)
 }
 
 func Login(ctx *fasthttp.RequestCtx) {
+	ctx.WriteString("Login")
 
 }
 
 func Register(ctx *fasthttp.RequestCtx) {
+	ctx.WriteString("Register")
 
 }
