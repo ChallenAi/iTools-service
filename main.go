87afe1eb8056ea923a61a46c48b88e38b7d3f0c1
ChallenAi <jianglun465@sina.com>
2018-09-16 03:23:08 +0800

    realize the middleware for fasthttprouter

--- main.go
+++ main.go
@@ -13,13 +13,22 @@ func PingPong(ctx *fasthttp.RequestCtx) {
 	ctx.WriteString("pong")
 }
 
+// Maybe there's a better way to realize a middleware
+func Logger(next fasthttp.RequestHandler) fasthttp.RequestHandler {
+	return fasthttp.RequestHandler(func(ctx *fasthttp.RequestCtx) {
+		fmt.Println(string(ctx.Method()) + " - " + ctx.URI().String())
+
+		next(ctx)
+	})
+}
+
 func main() {
 	router := fasthttprouter.New()
 	router.GET("/ping", PingPong)
 
-	router.GET("/api/users", controllers.GetAllUsers)
-	router.POST("/api/login", controllers.Login)
-	router.POST("/api/register", controllers.Register)
+	router.GET("/api/users", Logger(controllers.GetAllUsers))
+	router.POST("/api/login", Logger(controllers.Login))
+	router.POST("/api/register", Logger(controllers.Register))
 
 	fmt.Println("server is running at 9215...\n")
 	log.Fatal(fasthttp.ListenAndServe(":9215", router.Handler))
