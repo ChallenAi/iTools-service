bff3cd7cd837bb4565ad2bada51d57dd4ed7b536
ChallenAi <jianglun465@sina.com>
2018-09-15 23:16:33 +0800

    init router

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"github.com/buaazp/fasthttprouter"
+	"github.com/valyala/fasthttp"
+	"log"
 )
 
+type OkResp struct {
+	Code    int
+	Message string
+}
+
+func Index(ctx *fasthttp.RequestCtx) {
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	ctx.WriteString("hello")
+}
+
 func main() {
-	fmt.Println("hello, world")
+	router := fasthttprouter.New()
+	router.GET("/", Index)
+
+	log.Fatal(fasthttp.ListenAndServe(":9215", router.Handler))
 }
